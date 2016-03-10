13e7fb5e8cdc058843289bb863ccc292c686fb77
Evan Lovely <evan@evanlovely.com>
2016-03-10 09:50:14 -0800

    adding sidecar json overrides

--- index.js
+++ index.js
@@ -12,24 +12,34 @@ const config = {
   }
 };
 
-const data = require('./src/data/data.json');
+const globalData = require('./src/data/data.json');
 
 fs.emptyDirSync(config.dir.dist);
 
 glob.sync('**/*.twig', {
   cwd: config.dir.src
 }).forEach((file) => {
-  let html = twig({
-    namespaces: {
-      atoms: './src/atoms/',
-      molecules: './src/molecules/',
-      organisms: './src/organisms/'
-    },
-    data: fs.readFileSync(path.join(config.dir.src, file), 'utf8')
-  }).render(data);
-  
-  let newFilePath = path.join(config.dir.dist, file.replace('twig', 'html'));
-  
-  fs.mkdirsSync(path.dirname(newFilePath));
-  fs.writeFileSync(newFilePath, html);
+  let jsonPath = path.join(config.dir.src, file.replace('twig', 'json'));
+  fs.readFile(jsonPath, 'utf8', (err, localData) => {
+    // ok if no file there
+    let data = {};
+    Object.assign(data, globalData);
+    if (localData) {
+      Object.assign(data, JSON.parse(localData));
+    }
+    
+    let html = twig({
+      namespaces: {
+        atoms: './src/atoms/',
+        molecules: './src/molecules/',
+        organisms: './src/organisms/'
+      },
+      data: fs.readFileSync(path.join(config.dir.src, file), 'utf8')
+    }).render(data);
+
+    let newFilePath = path.join(config.dir.dist, file.replace('twig', 'html'));
+
+    fs.mkdirsSync(path.dirname(newFilePath));
+    fs.writeFileSync(newFilePath, html);
+  });
 });
