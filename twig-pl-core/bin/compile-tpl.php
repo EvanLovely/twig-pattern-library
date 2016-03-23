64d7200cd5a7cefa1d2d90b4864df6a8e6a26646
Evan Lovely <evan@evanlovely.com>
2016-03-23 11:40:55 -0700

    setting up ability to pass in config file as arg, making some tests

--- twig-pl-core/bin/compile-tpl.php
+++ twig-pl-core/bin/compile-tpl.php
@@ -6,27 +6,43 @@ use Symfony\Component\Finder\Finder;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
-$srcPath = __DIR__.'/../src/';
-
 $fs = new Filesystem();
 $yamlParser = new Parser();
-$loader = new Twig_Loader_Filesystem($srcPath);
-$loader->addPath($srcPath . 'atoms', 'atoms');
-$loader->addPath($srcPath . 'molecules', 'molecules');
-$loader->addPath($srcPath . 'organisms', 'organisms');
+
+// Getting config file
+// The first argument to this script is a path to a configuration yaml file; relative to CWD.
+$configFilePath = $argv[1];
+$configFileDir = dirname($configFilePath);
+$config = $yamlParser->parse(file_get_contents($configFilePath));
+// changing working directory to where config file is, so we can use it's relative paths
+chdir($configFileDir);
+
+$loader = new Twig_Loader_Filesystem($config['src']);
+// @todo Dynamically look at all folders in `$config['src']` and add them via `$loader-addPath()` so Twig knows of them.
+//$loader->addPath($config['src'] . 'atoms', 'atoms');
+//$loader->addPath($config['src'] . 'molecules', 'molecules');
+//$loader->addPath($config['src'] . 'organisms', 'organisms');
 $finder = new Finder();
+
 $twig = new Twig_Environment($loader, array(
-  'cache' => './cache',
+  'cache' => __DIR__ . '/cache', // `__DIR__` is still the directory where this file resides
 ));
 
-$globalData = $yamlParser->parse(file_get_contents(__DIR__.'/../src/data/data.yml'));
+// $config['globalData'] is an array of file paths to data files, we'll merge them all together
+$globalData = array();
+if (isset($config['globalData'])) {
+    foreach ($config['globalData'] as $file) {
+        $chunk = $yamlParser->parse(file_get_contents($file));
+        $globalData = array_merge($globalData, $chunk);
+    }
+}
 
-$finder->files()->in(__DIR__.'/../src')->name('*.twig');
+$finder->files()->in($config['src'])->name('*.twig');
 
 foreach ($finder as $file) {
   $path = $file->getRelativePathname();
   
-  $localDataPath = $srcPath.str_replace(".twig", ".yml", $path);
+  $localDataPath = $config['src'] . str_replace(".twig", ".yml", $path);
   if ($fs->exists($localDataPath)) {
     $localData = $yamlParser->parse(file_get_contents($localDataPath));
     $data = array_merge($globalData,$localData);
@@ -37,7 +53,7 @@ foreach ($finder as $file) {
 
   $template = $twig->loadTemplate($path);
   $html = $template->render($data);
-  $fileDest = '../dist/' . str_replace(".twig", ".html", $path);
+  $fileDest = $config['dist'] . str_replace(".twig", ".html", $path);
 
   try {
     $fs->mkdir(dirname($fileDest));
@@ -47,5 +63,3 @@ foreach ($finder as $file) {
   }
 
 }
-
-?>
